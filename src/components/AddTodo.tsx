6a384c8515c3ba24ab22610ab0d77ae99a7105af
rishuverma2 <rishu.verma@shell.com>
2023-06-06 14:04:32 +0530

    useRef to focus on Input box and changed title

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addTodo } from "../features/todoSlice";
 
 const AddTodo = () => {
   const todo = useSelector((state: any) => state.todo);
+  const inputValRef = useRef<HTMLInputElement>(null);
   const [inputVal, setInputVal] = useState(todo.textInInputBox);
   const dispatch = useDispatch();
   const handleSubmit = (event: any) => {
@@ -16,16 +17,18 @@ const AddTodo = () => {
       );
     setInputVal("");
   };
-  useEffect(
-    () => setInputVal(todo.textInInputBox),
-    [todo.textInInputBox, todo.todoArray]
-  );
+  useEffect(() => {
+    if (inputValRef.current) inputValRef.current.focus();
+    setInputVal(todo.textInInputBox);
+  }, [todo]);
+
   return (
     <>
       <form onSubmit={handleSubmit} className="d-flex mb-3 ">
         <input
           className="form-control form-control-lg"
           type="text"
+          ref={inputValRef}
           value={inputVal}
           placeholder="Add Todo here..."
           onChange={(event) => setInputVal(event.target.value)}
