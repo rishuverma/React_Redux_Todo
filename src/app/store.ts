43188788ab1126006a32a3ea4f23648228e03c95
rishuverma2 <rishu.verma@shell.com>
2023-06-12 17:51:05 +0530

    used rtk query to fetch data from dummyjson.com site

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "../features/todoSlice";
+import { apiSlice } from "../features/api/apiSlice";
 
 const store = configureStore({
   reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
     todo: todoReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
 export default store;
--- src/features/todoSlice.ts
+++ src/features/todoSlice.ts
@@ -3,9 +3,8 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   textInInputBox: "",
   todoArray: [
-    { id: 1, todoTitle: "todo Item 1", completed: false },
-    { id: 2, todoTitle: "todo item 2", completed: false },
-    { id: 3, todoTitle: "todo item 3", completed: true },
+    { id: Date.now(), todoTitle: "todo Item 1", completed: false },
+    // { id: Date.now() + 1, todoTitle: "todo item 2", completed: false },
   ],
 };
 
@@ -15,9 +14,9 @@ const todoSlice = createSlice({
   reducers: {
     addTodo: (state, action) => {
       const newTodoObject = {
-        id: Date.now(),
+        id: action.payload.id || Date.now(),
         todoTitle: action.payload.todoTitle,
-        completed: false,
+        completed: action.payload.completed || false,
       };
       state.textInInputBox = "";
       state.todoArray.push(newTodoObject);
